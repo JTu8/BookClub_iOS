a84ec77d4dda51e7e7623925742ca82088f10f9d
JTu8 <htmjaet8@gmail.com>
2019-04-15 13:52:55 +0300

    Näkymät lisätty ja navigointi toimii

---
+++ BookClub/AddBook.swift
@@ -0,0 +1,31 @@
+//
+//  AddBook.swift
+//  BookClub
+//
+//  Created by Jere Tuppurainen on 22.3.2019.
+//  Copyright © 2019 Jere Tuppurainen. All rights reserved.
+//
+
+import UIKit
+
+class AddBook: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Add new book"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
